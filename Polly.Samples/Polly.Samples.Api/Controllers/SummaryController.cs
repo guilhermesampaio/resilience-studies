44f6ddf89b6b2ea2f8ff420bcbf7a31cd343013f
guilherme <santos.gsampaio@gmail.com>
2019-10-23 01:03:29 -0300

    Implement simple retry

--- Polly.Samples/Polly.Samples.Api/Controllers/SummaryController.cs
+++ Polly.Samples/Polly.Samples.Api/Controllers/SummaryController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Polly.Retry;
 
 namespace Polly.Samples.Api.Controllers
 {
@@ -14,24 +15,33 @@ namespace Polly.Samples.Api.Controllers
     public class SummaryController : ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _httpRetryPolicy;
 
         public SummaryController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
+            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(it => !it.IsSuccessStatusCode)
+                .RetryAsync(3);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetPostion()
         {
+            // Retry
+            // Retry with wait
+            // Circuit Breaker
+            // Fallback
+
             var client = _httpClientFactory.CreateClient();
 
-            var uriBase = "http://localhost:6000/";
+            var uriBase = "http://localhost:5000/";
             var path = "position";
             var endpoint = $"{uriBase}{path}";
             var uri = new Uri(endpoint);
 
-            var response = await client.GetAsync(uri);
 
+            var response = await _httpRetryPolicy.ExecuteAsync(() => client.GetAsync(uri));
+            
             if (response.IsSuccessStatusCode)
             {
                 return Ok(response.Content.ReadAsStringAsync());
