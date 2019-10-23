63068ab290efd09417a6d2a6274ac4b1324de3e0
guilherme <santos.gsampaio@gmail.com>
2019-10-23 00:43:37 -0300

    Add project

---
+++ Polly.Samples/Polly.Samples.Api/Controllers/PositionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Polly.Samples.Position.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PositionController : ControllerBase
+    {
+
+        private static int _requests = 0;
+
+
+
+        [HttpGet]
+        public IActionResult GetPostion()
+        {
+            _requests++;
+
+            if (_requests >= 2 && _requests <= 4)
+                return StatusCode((int)HttpStatusCode.ServiceUnavailable, "Service Unvailable");
+
+            return Ok(35690.89M);
+        }
+    }
+}
---
+++ Polly.Samples/Polly.Samples.Api/Controllers/SummaryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Polly.Samples.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SummaryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPostion()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var uriBase = "http://localhost:6000/";
+            var path = "position";
+            var endpoint = $"{uriBase}{path}";
+            var uri = new Uri(endpoint);
+
+            var response = await client.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(response.Content.ReadAsStringAsync());
+            }
+
+            return StatusCode((int)response.StatusCode);
+        }
+    }
+}
