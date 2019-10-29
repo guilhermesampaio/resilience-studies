60c6649615b6f24c7035758351d9310c71271469
guilherme <santos.gsampaio@gmail.com>
2019-10-29 01:55:56 -0300

    Circuitbreaker with fallback tests

--- Polly.Samples/Polly.Samples.Api/Controllers/PositionController.cs
+++ Polly.Samples/Polly.Samples.Api/Controllers/PositionController.cs
@@ -8,19 +8,31 @@ namespace Polly.Samples.Position.Controllers
     public class PositionController : ControllerBase
     {
 
-        private static int _requests = 0;
+        private static int _positionRequests = 0;
+        private static int _custodyRequests = 0;
 
-
-
-        [HttpGet]
+        [HttpGet("position")]
         public IActionResult GetPostion()
         {
-            _requests++;
+            _positionRequests++;
 
-            if (_requests >= 2 && _requests <= 4)
+            if (_positionRequests >= 2 && _positionRequests <= 4)
                 return StatusCode((int)HttpStatusCode.ServiceUnavailable, "Service Unvailable");
 
             return Ok(35690.89M);
+        }
+
+        [HttpGet("custody")]
+        public IActionResult GetCustody() 
+        {
+            _custodyRequests++;
+
+            if(_custodyRequests % 5 == 0)
+                return StatusCode((int)HttpStatusCode.GatewayTimeout);
+
+            return Ok();
+
+
         }
     }
 }
--- Polly.Samples/Polly.Samples.Api/Controllers/SummaryController.cs
+++ Polly.Samples/Polly.Samples.Api/Controllers/SummaryController.cs
@@ -6,48 +6,79 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Polly.CircuitBreaker;
+using Polly.Fallback;
 using Polly.Retry;
+using Polly.Wrap;
 
 namespace Polly.Samples.Api.Controllers
 {
+    /// <summary>
+    /// Objetivo de simular os seguintes cenários
+    /// Retry
+    // Retry with wait
+    // Circuit Breaker
+    // Fallback
+    /// </summary>
     [Route("[controller]")]
     [ApiController]
     public class SummaryController : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
+
         private readonly AsyncRetryPolicy<HttpResponseMessage> _httpRetryPolicy;
+                
+        private static readonly AsyncCircuitBreakerPolicy<HttpResponseMessage> _breakerPolicy = 
+            Policy
+                .HandleResult<HttpResponseMessage>(it => !it.IsSuccessStatusCode)
+                .CircuitBreakerAsync(1, TimeSpan.FromSeconds(10));
+
+        private static readonly AsyncFallbackPolicy<HttpResponseMessage> _fallBackPolicy = 
+            Policy<HttpResponseMessage>
+                .Handle<BrokenCircuitException>()                
+                .FallbackAsync(new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.BadRequest });
+
+        private const string _baseUri = "http://localhost:5000";
 
         public SummaryController(IHttpClientFactory httpClientFactory)
         {
-            _httpClientFactory = httpClientFactory;
-            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(it => !it.IsSuccessStatusCode)
+            _httpClient = httpClientFactory.CreateClient();
+
+            _httpRetryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(it => !it.IsSuccessStatusCode)
                 .RetryAsync(3);
         }
 
-        [HttpGet]
+        [HttpGet("position")]
         public async Task<IActionResult> GetPostion()
         {
-            // Retry
-            // Retry with wait
-            // Circuit Breaker
-            // Fallback
+            var positionPath = $"{_baseUri}/position/position";
+            var postionEndpoint = new Uri(positionPath);
 
-            var client = _httpClientFactory.CreateClient();
+            var response = await _httpRetryPolicy.ExecuteAsync(() => _httpClient.GetAsync(postionEndpoint));
 
-            var uriBase = "http://localhost:5000/";
-            var path = "position";
-            var endpoint = $"{uriBase}{path}";
-            var uri = new Uri(endpoint);
-
-
-            var response = await _httpRetryPolicy.ExecuteAsync(() => client.GetAsync(uri));
-            
             if (response.IsSuccessStatusCode)
             {
                 return Ok(response.Content.ReadAsStringAsync());
             }
 
             return StatusCode((int)response.StatusCode);
+        }
+
+        [HttpGet("custody")]
+        public async Task<IActionResult> GetCustody()
+        {
+            var custodyPath = $"{_baseUri}/position/custody";
+            var custodyEndpoint = new Uri(custodyPath);
+
+            //var response = await _fallBackPolicy
+            //    .WrapAsync(_breakerPolicy)
+            //    .ExecuteAsync(() => _httpClient.GetAsync(custodyEndpoint));
+
+            var response = await _breakerPolicy
+                .ExecuteAsync(() => _httpClient.GetAsync(custodyEndpoint));
+
+            return StatusCode((int)response.StatusCode, new { });
         }
     }
 }
